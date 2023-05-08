66928e5d3afd1dacbf6e431d82e099e6d8d91608
Akas_Datta <012akasdatta@gmail.com>
2023-05-08 23:26:49 +0600

    added users from index.js

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [ 
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com'},
+    { id: 1, name: 'sabnoor', email: 'sabnoor@gmail.com'},
+    { id: 1, name: 'alomgir', email: 'alomgir@gmail.com'}
+]
+
 app.get('/', (req, res) => {
     res.send('Users management server is running')
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.listen(port, ()=> {
